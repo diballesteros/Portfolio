51367f2cc2bd2e930fbc39da97709826d46ef9c0
Diego Ballesteros <diballesterosc@gmail.com>
2021-03-15 09:42:19 -0500

    Dependency updates, button component style changes, changed to css

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,23 +1,18 @@
 import React from "react"
-import PropTypes from "prop-types"
-import styles from "./Button.module.scss"
+
+import "./Button.css"
 
 interface ButtonProps {
-  click: () => void
+  click?: () => void
   text: string
 }
 
 const Button: React.FC<ButtonProps> = ({ click, text }) => {
   return (
-    <button className={styles.button} onClick={click} type="button">
+    <button className="button" onClick={click} type="button">
       {text}
     </button>
   )
-}
-
-Button.propTypes = {
-  click: PropTypes.func.isRequired,
-  text: PropTypes.string.isRequired,
 }
 
 export default Button
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,26 @@
 import React from "react"
 import Button from "../components/Button/Button"
-import styles from "./index.module.scss"
+import "./index.css"
 
 const Home: React.FC = () => {
   return (
-    <main className={styles.main}>
-      <div className={`${styles.title} ${styles.page}`}>
-        <div className={styles.container}>
+    <main className="main">
+      <section className="title page">
+        <div className="container">
           <h1>
-            Hi, my name is{" "}
-            <span className={styles.main_color}>Diego Ballesteros</span>
+            Hi, my name is <span className="main_color">Diego Ballesteros</span>
           </h1>
           <h1>I&apos;m a Front End Developer</h1>
           <Button click={null} text="Know More" />
         </div>
-      </div>
+      </section>
+      <section className="aboutme">
+        <h2>ABOUT ME</h2>
+        <div className="container">
+          <div>picture</div>
+          <p>test</p>
+        </div>
+      </section>
     </main>
   )
 }
