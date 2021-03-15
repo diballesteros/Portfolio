97df7c5f965cf371d0b882a3818d01eccec066b6
Diego Ballesteros <diballesterosc@gmail.com>
2021-03-15 15:23:35 -0500

    Initial aboutme component, profile picture, index style fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,15 @@
 import React from "react"
-import Button from "../components/Button/Button"
-import "./index.css"
+import NavBar from "../components/NavBar/NavBar"
+import AboutMe from "./AboutMe/AboutMe"
+import "./index.scss"
 
 const Home: React.FC = () => {
   return (
     <main className="main">
-      <section className="title page">
-        <div className="container">
-          <h1>
-            Hi, my name is <span className="main_color">Diego Ballesteros</span>
-          </h1>
-          <h1>I&apos;m a Front End Developer</h1>
-          <Button click={null} text="Know More" />
-        </div>
-      </section>
-      <section className="aboutme">
-        <h2>ABOUT ME</h2>
-        <div className="container">
-          <div>picture</div>
-          <p>test</p>
-        </div>
-      </section>
+      <NavBar />
+      <div className="body">
+        <AboutMe />
+      </div>
     </main>
   )
 }
