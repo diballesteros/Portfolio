be3bb173a88aef27a3f6d001d9d2e93caf785e1a
Diego Ballesteros <diballesterosc@gmail.com>
2021-03-15 10:08:57 -0500

    Test commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Button from "../components/Button/Button"
+import Button from "src/components/Button/Button"
 import "./index.css"
 
 const Home: React.FC = () => {
